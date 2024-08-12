92723388224c18fc351cbd67c74e7349890e2808
debajyoti-jpg <mallikdebajyoti@gmail.com>
2024-08-12 14:45:22 +0530

    All commits

--- src/main/java/org/example/org/example/StringPractice/TestClass.java
+++ src/main/java/org/example/org/example/StringPractice/TestClass.java
@@ -5,7 +5,7 @@ public class TestClass {
 
     public static void main(java.lang.String[] args) {
 
-    //reverse of a string
+        //reverse of a string
 
         java.lang.String concat = "";
         for(int i=name.length()-1;i>=0;i--){
@@ -14,17 +14,17 @@ public class TestClass {
             concat+=name.charAt(i);
         }
         System.out.println("Traditional Way :"+concat);
-       // to find all employee who haven't submit the timesheet
+        // to find all employee who haven't submit the timesheet
 
 //        Select Employee_Name from Employee_table where id IN (Select empliyee_id from TimeSheet_table where
 //
 //        timeshee_value IS null);
         StringBuilder st  = new StringBuilder(name);
         st.reverse();
-    System.out.println("String Buffer Way :"+st);
+        System.out.println("String Buffer Way :"+st);
 
-    java.lang.String rev = name.chars().mapToObj(ch -> java.lang.String.valueOf((char) ch)).
-            reduce((acc,ch) -> ch+acc).orElse("");
+        java.lang.String rev = name.chars().mapToObj(ch -> java.lang.String.valueOf((char) ch)).
+                reduce((acc,ch) -> ch+acc).orElse("");
 
         System.out.println("Java 8  Way :"+rev);
     }
